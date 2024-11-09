5eb1db1dba36de5408820ac59427184b5876a1f1
zxcwsurx <102683665+zxcwsurx@users.noreply.github.com>
2024-11-09 22:38:30 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+rm -rf device/xiaomi
+rm -rf kernel/xiaomi
+rm -rf vendor/xiaomi
+rm -rf hardware/xiaomi
+
+# Cleanup to fix SyncErrors raised during branch checkouts
+rm -rf platform/prebuilts
+rm -rf prebuilts
+
+
+echo "========================================================================"
+echo "DELETED DIRECTORIES"
+echo "========================================================================"
+
+
+# Repo Init
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs--depth=1
+
+echo "========================================================================"
+echo "REPO INITIALIZED"
+echo "========================================================================"
+
+
+# Clone local_manifests repository
+git clone https://github.com/zxcwsurx/android_device_xiaomi_nabu device/xiaomi/nabu
+git clone https://github.com/Dark-Matter7232/kernel_msm-4.14-nabu kernel/xiaomi/nabu
+git clone https://github.com/zxcwsurx/android_vendor_xiaomi_nabu vendor/xiaomi/nabu
+git clone https://github.com/zxcwsurx/android_device_xiaomi_sm8150-common device/xiaomi/sm8150-common
+git clone https://github.com/xiaomi-sm8150-devs/proprietary_vendor_xiaomi_sm8150-common vendor/xiaomi/sm8150-common
+
+echo "========================================================================"
+echo "CLONED REPOS"
+echo "========================================================================"
+
+
+# Resync
+
+/opt/crave/resync.sh
+
+echo "========================================================================"
+echo "RESYNCED"
+echo "========================================================================"
+
+
+# Upgrade System
+
+sudo apt update && sudo apt upgrade -y
+
+echo "========================================================================"
+echo "SYSTEM UPGRADED"
+echo "========================================================================"
+
+
+rm -rf packages/apps/Settings
+git clone -b 14.0 https://github.com/zxcwsurx/android_packages_apps_Settings/
+
+echo "========================================================================"
+echo "BUILDING........."
+echo "========================================================================"
+
+
+# Set some environment variables
+export TZ=Ukraine/Lviv
+export BUILD_USERNAME=Hxfuxyy
+export BUILD_HOSTNAME=hxfuxyy
+
+
+# Lunch
+. build/envsetup.sh
+brunch nabu
