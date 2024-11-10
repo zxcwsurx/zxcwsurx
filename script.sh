461bed0007194db8b7cd669471483d8f367bef99
zxcwsurx <102683665+zxcwsurx@users.noreply.github.com>
2024-11-10 13:33:36 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,68 +1,32 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
-rm -rf device/xiaomi
-rm -rf kernel/xiaomi
-rm -rf vendor/xiaomi
-rm -rf hardware/xiaomi
-
-# Cleanup to fix SyncErrors raised during branch checkouts
-rm -rf platform/prebuilts
-rm -rf prebuilts
+rm -rf .repo/local_manifests/
 
 
-echo "========================================================================"
-echo "DELETED DIRECTORIES"
-echo "========================================================================"
+# Initialize local repository
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
+# Local manifests
+git clone https://github.com/zxcwsurx/local_manifests_nabu -b crdroid-14 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
-# Repo Init
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs --depth=1
+# Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
 
-echo "========================================================================"
-echo "REPO INITIALIZED"
-echo "========================================================================"
-
-
-# Resync
-
-echo "========================================================================"
-echo "SYNCING"
-echo "========================================================================"
-
-repo sync -j$(nproc --all)
-
-echo "========================================================================"
-echo "SYNCED"
-echo "========================================================================"
-
-
-
-echo "========================================================================"
-echo "CLONING REPOS"
-echo "========================================================================"
-
-# Clone local_manifests repository
-git clone https://github.com/zxcwsurx/android_device_xiaomi_nabu device/xiaomi/nabu
-git clone https://github.com/Dark-Matter7232/kernel_msm-4.14-nabu kernel/xiaomi/nabu
-git clone https://github.com/zxcwsurx/android_vendor_xiaomi_nabu vendor/xiaomi/nabu
-git clone https://github.com/zxcwsurx/android_device_xiaomi_sm8150-common device/xiaomi/sm8150-common
-git clone https://github.com/xiaomi-sm8150-devs/proprietary_vendor_xiaomi_sm8150-common vendor/xiaomi/sm8150-common
-rm -rf packages/apps/Settings
-git clone -b 14.0 https://github.com/zxcwsurx/android_packages_apps_Settings/
-
-
-echo "========================================================================"
-echo "BUILDING........."
-echo "========================================================================"
-
-
-# Set some environment variables
-export TZ=Ukraine/Lviv
+# Export
 export BUILD_USERNAME=Hxfuxyy
-export BUILD_HOSTNAME=hxfuxyy
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
 
-
-# Lunch
+# Lunch  
 . build/envsetup.sh
 brunch nabu
+
